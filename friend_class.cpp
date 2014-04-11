5e33504bff2c1b669c931cc6cdb5681b718130c4
Chenthill Palanisamy <chenthillrulz@gmail.com>
2014-04-11 13:55:24 +0530

    Remove delete binary tree operation as it does not cover all cases

--- friend_class.cpp
+++ friend_class.cpp
@@ -50,9 +50,6 @@ class BinaryTree
 
 		Node * find_suitable_parent (Node *parent, int key);
 		Node * find_node (Node *node, Node **parent, int key);
-		Node * find_inorder_successor_node (Node *node, Node **parent);
-		bool remove_leaf (Node *node, Node *parent);
-		bool remove_node_with_one_child (Node *node);
 		void in_order_traverse (Node *node, ostream &out);
 
 	public:
@@ -62,12 +59,10 @@ class BinaryTree
 
 		bool get_data (int key, int *data);
 		void insert (int key, int data);
-		bool remove (int key);
 		~BinaryTree ();
 
 		friend ostream & operator<< (ostream &, BinaryTree *tree);
 		BinaryTree  operator+ (int key);
-		BinaryTree  operator- (int key);
 };
 
 
@@ -122,103 +117,6 @@ Node * BinaryTree::find_node (Node *node, Node **parent, int key)
 	return NULL;
 }
 
-Node * BinaryTree::find_inorder_successor_node (Node *node, Node **parent)
-{
-	if (node->left == NULL)
-		return node;
-
-	*parent = node;
-	return find_inorder_successor_node (node->left, parent);
-}
-
-bool BinaryTree::remove_leaf (Node *node, Node *parent)
-{
-	// In case node is a leaf node
-	if (node->isLeaf ())
-	{
-		// its a root node
-		if (parent == NULL) 
-		{
-			root = NULL;
-			delete node;
-		} else 
-		{
-			if (parent->left == node)
-				parent->left = NULL;
-			else
-				parent->right = NULL;
-
-			delete node;
-		}
-
-		return true;
-	}
-
-	return false;
-}
-
-bool BinaryTree::remove_node_with_one_child (Node *node)
-{
-	// In case node has only one child, replace it with the child
-	if (node->left == NULL || node->right == NULL)
-	{
-		Node *repl;
-
-		if (node->left == NULL)
-			repl = node->right;
-		else
-			repl = node->left;
-		
-		node->key = repl->key;
-		node->data = repl->data;
-		
-		node->left = repl->left;
-		node->right = repl->right;
-
-		delete repl;
-		
-		return true;
-	}
-
-	return false;
-}
-
-// Ideally we would want to return the node being deleted for the users to handle 
-// data, key destruction
-bool BinaryTree::remove (int key)
-{
-	Node *parent = NULL;
-	Node *node = find_node (root, &parent, key);
-
-	if (node == NULL)
-		return false; // or maybe throw an exception
-
-	if (remove_leaf (node, parent))
-		return true;
-	
-	if (remove_node_with_one_child (node))
-		return true;
-
-	// In case node has two children, find the inorder successor node and replace it with
-	// with the current node. delete the inorder successor node	
-	{
-		parent = node;
-		
-		// finds the left most node of node->right
-		Node *in_order_suc = find_inorder_successor_node (node->right, &parent); 
-		
-		node->key = in_order_suc->key;
-		node->data = in_order_suc->data;
-
-		if (in_order_suc->isLeaf ())
-			remove_leaf (in_order_suc, parent);
-		else
-			remove_node_with_one_child (in_order_suc);
-	}
-
-	return true;
-}
-
 bool BinaryTree::get_data (int key, int *data)
 {
 	Node *parent = NULL;
@@ -241,13 +139,6 @@ BinaryTree::~BinaryTree ()
 BinaryTree  BinaryTree::operator+ (int key)
 {
 	insert(key, 1234);
-
-	return *this;
-}
-
-BinaryTree BinaryTree::operator- (int key)
-{
-	remove (key);
 
 	return *this;
 }
@@ -289,19 +180,9 @@ int main ()
 	//Tree with data inserted
 	cout << tree;
 
-	int temp = a[(rand() % NumElements)];
-	cout << "Removing " << temp << "\n" << endl;
-	tree->remove (temp);
-	cout << tree;
-
 	// Inserting an element using a fancy overloaded operator
 	cout << "Inserting 799 using our fancy overloaded operator for fun ;) \n" << endl;
 	(*tree) + 799;
-	cout << tree;
-
-	// Deleting an element using a fancy overloaded operator
-	cout << "Removing 799 using our fancy overloaded operator for fun ;) \n" << endl;
-	(*tree) - 799;
 	cout << tree;
 
 	std::cout << "Demonstrating friend class and friend functions" << std::endl;
